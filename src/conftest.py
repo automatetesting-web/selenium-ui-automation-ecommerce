84bd06839e493b704c5f5a71a6975aea5fadca74
automatetesting-web <testing.for.asset.h@gmail.com>
2026-05-15 10:24:37 +0530

    test-report

--- src/conftest.py
+++ src/conftest.py
@@ -11,3 +11,14 @@ def driver():
     yield driver
     driver.quit()
     
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver")
+        if driver:
+            driver.save_screenshot("failure.png")
+``
